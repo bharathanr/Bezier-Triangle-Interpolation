d38b8e2de9c31c147e615f534747e6102615f028
batbrat <bharathan100@gmail.com>
2012-05-10 18:34:09 -0600

    Author: Bharathan Rajaram Date: 10th May 2012 Class Project: CS 6680 Spring 2012 C.A.G.D. II taught by Prof. Elaine Cohen Rewrite of all previous code for the Bezier Triangle Interpolation project. I'm forced to start from scratch. I'm using modern OpenGL.

---
+++ CS6680_Bezier_Triangle_Interpolation_2/CS6680_Bezier_Triangle_Interpolation_2.cpp
@@ -0,0 +1,11 @@
+// CS6680_Bezier_Triangle_Interpolation_2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
